7bba2482ce76e230b5a25f0994dba7b93856e83d
KabaevNV <nkkab2@mail.ru>
2025-09-17 20:23:01 +0300

    Перенес данные для подключения базы данных в .env

--- connect_to_db.py
+++ connect_to_db.py
@@ -1,35 +1,27 @@
 import os
 import psycopg2
 
-from configparser import ConfigParser
+from dotenv import load_dotenv
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-db_ini_path = os.path.join(script_dir, 'database.ini')
+load_dotenv()
 
-def load_config(filename='database.ini', section='postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
-
-    config = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            config[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return config
+db_host = os.getenv("HOST")
+db_name = os.getenv("DATABASE")
+db_user = os.getenv("USER")
+db_password = os.getenv("PASSWORD")
 
 def connect_to_db():
-    config = load_config(db_ini_path)
     try:
-        with psycopg2.connect(**config) as conn:
+        with psycopg2.connect(
+            host=db_host,
+            dbname=db_name,
+            user=db_user,
+            password=db_password
+        ) as conn:
             print('Connected to the PostgreSQL server.')
             return conn
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
 
 if __name__ == '__main__':
-    filename = db_ini_path
-    config = load_config(filename)
-    print(config)
+    connect_to_db()
