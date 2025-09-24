0af8e9bdd57c126de42d464e9f03f51fbc2073b3
KabaevNV <nkkab2@mail.ru>
2025-09-24 14:47:58 +0300

    Добавил основную страницу и прикрутил добавление и обновление камер к ней

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Request, Depends, Form
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 
-from .database import db, get_user_by_username, create_user
+from .database import (
+    db, 
+    get_user_by_username, 
+    create_user, 
+    get_camera,
+    check_user_camera,
+    get_camera_by_id,
+    get_user_cameras,
+    add_camera,
+    update_camera_db)
 from .security import (
     get_password_hash, 
     create_access_token, 
@@ -19,7 +28,8 @@ async def lifespan(app: FastAPI):
     await db.connect()
     yield
     await db.disconnect()
-    
+
+
 app = FastAPI(lifespan=lifespan)
 templates = Jinja2Templates(directory="app/templates")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
@@ -99,9 +109,53 @@ async def login(
             }
         )
     token = create_access_token(username)
-    response = RedirectResponse(url="/users/me", status_code=302)
+    response = RedirectResponse(url="/panel", status_code=302)
     response.set_cookie(key="access_token", value=token, httponly=True)
     return response
+
+@app.get("/panel", response_class=HTMLResponse)
+async def panel_form(request: Request,
+                     user: User = Depends(get_current_user)):
+    cameras = await get_user_cameras(user.id)
+    if cameras:
+        cameras_list = [dict(cam) for cam in cameras]
+    else:
+        cameras_list = []
+
+    return templates.TemplateResponse("main.html", {"request": request, 
+                                                    "cameras": cameras_list})
+
+@app.get("/panel", response_class=HTMLResponse)
+async def panel(current_user: User = Depends(get_current_user)):
+    return current_user
+
+@app.post("/cameras/add")
+async def add_new_camera(name: str = Form(...),
+                         url: str = Form(...),
+                         user: User = Depends(get_current_user)):
+    camera = await get_camera(user.id, name)
+    if camera:
+        return JSONResponse({"success": False, "error": "Камера уже существует"})
+    
+    camera_id = await add_camera(user.id, name, url)
+    return JSONResponse({"success": True, "id": camera_id})
+
+@app.post("/cameras/update")
+async def update_camera(id: int = Form(...),
+                        name: str = Form(...),
+                        url: str = Form(...),
+                        user: User = Depends(get_current_user)):
+    if await check_user_camera(user.id, id):
+        result = await update_camera_db(id, name, url)
+        print(result)
+        if result:
+            return JSONResponse({"success": True, "id": id})
+        else:
+            return JSONResponse({"success": False, 
+                                 "error": "Не удалось обновить бд"})
+
+    return JSONResponse({"success": False, 
+                         "error": "Неавторизованное обновление бд"})
 
 @app.get("/users/me", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_user)):
